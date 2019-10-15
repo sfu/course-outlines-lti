e27c927fe113e170c11e6b82c2b9c7007787f5b4
Graham Ballantyne <grahamb@sfu.ca>
2019-10-15 16:39:44 -0700

    Add headerMunger
    AWS sends some headers as all-lowercase (`accept`, `content-type`) and others as mixed-case (`Host`, `X-Forwarded-Proto`). Normalize them to all-lowercase, preserving the mixed-case ones.

---
+++ src/shared/headerMunger.js
@@ -0,0 +1,18 @@
+// AWS sends some request headers as all-lower-case, and others as mixed-case. Normalize them for great justice.
+// Hat-tip:
+// https://architecture-as-text.slack.com/archives/C6BGT0D08/p1571179244177200?thread_ts=1571178938.175700&cid=C6BGT0D08
+
+module.exports = req => {
+  const headerKeys = Object.keys(req.headers);
+  const normalizedHeaders = {
+    ...req.headers,
+  };
+
+  headerKeys.forEach(header => {
+    const lcHeader = header.toLowerCase();
+    if (!req.headers.hasOwnProperty(lcHeader)) {
+      normalizedHeaders[lcHeader] = req.headers[header];
+    }
+  });
+  req.headers = normalizedHeaders;
+};
